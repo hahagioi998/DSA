b306013fed8d6445efdff927eb7ac4852a07ac3e
koshishrijal <koshishrijal468@gmail.com>
2022-03-30 17:11:32 +0545

    Binary search recursive method added

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -4,8 +4,8 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int arr[] = {1, 25, -20, 10, 23, 52, 18};
-        System.out.println(iterativeBinarySearch(arr,10));
-        System.out.println(iterativeBinarySearch(arr,80));
+        System.out.println(binarySearch(arr, 10));
+        System.out.println(binarySearch(arr, 80));
     }
 
     public static int iterativeBinarySearch(int[] arr, int searchValue) {
@@ -23,5 +23,25 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    public static int binarySearch(int[] arr, int searchValue) {
+        return recursiveBinarySearch(arr, 0, arr.length, searchValue);
+    }
+
+    private static int recursiveBinarySearch(int[] arr, int start, int end, int searchValue) {
+        if (start >= end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] == searchValue) {
+            return mid;
+        } else if (arr[mid] < searchValue) {
+            start = mid + 1;
+            return recursiveBinarySearch(arr, start, end, searchValue);
+        } else {
+            end = mid;
+            return recursiveBinarySearch(arr, start, end, searchValue);
+        }
     }
 }
